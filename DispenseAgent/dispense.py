30a2ddb8d853d418c3b44dd663c1a925bc6cf060
Sathvik <s.sathvik23@gmail.com>
2025-03-12 21:07:56 +0530

    Parsing basic necessary attr

--- DispenseAgent/dispense.py
+++ DispenseAgent/dispense.py
@@ -1,27 +1,39 @@
 import json
-import ast
+import pprint
 
 
 with open("sample.json", "r") as file:
-    raw_data = file.read()
-
-data = ast.literal_eval(raw_data)
-
-json_data = json.loads(json.dumps(data))
+    data = json.load(file)
 
 activity_data = {
     "activity_type": data.get("type", "Unknown"),
     "distance_km": data.get("distance", 0) / 1000,  
-    "average_speed_mps": data.get("average_speed", 0),  
     "average_speed_kmh": data.get("average_speed", 0) * 3.6,  
     "sport_type": data.get("sport_type", "Unknown"),
     "athlete_count": data.get("athlete_count", 1),
     "calories": data.get("calories", 0),
     "commute": data.get("commute", False),
-
+    "total_elevation_gain": data.get("total_elevation_gain", 0),
+    "start_date_local": data.get("start_date_local", "Unknown"),
+    "achievement_count": data.get("achievement_count", 0),
+    "start_latlng": data.get("start_latlng", [0, 0]),
+    "device_name": data.get("device_name", "Unknown"),
+}
+activity_data["gear_info"] = {
+    "gear_name": data.get("gear", {}).get("name", "Unknown"),
+    "gear_nickname": data.get("gear", {}).get("nickname", "Unknown")
 }
 
-if data.get("device_watts", False):
-    activity_data["average_watts"] = data.get("average_watts", 0)
+activity_data["splits"] = [
+    {
+        "distance": item.get("distance"),
+        "elevation_difference": item.get("elevation_difference"),
+        "moving_time": item.get("moving_time")
+    }
+    for item in data.get("splits_metric", [])
+]
 
-print(activity_data)
+if data.get("device_watts", False):
+    activity_data["average_watts"] = data.get("average_watts", "Unknown")
+
+pprint.pprint(activity_data)
